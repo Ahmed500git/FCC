3fae462890113b3600778e160ae7d4be006b0721
ahmed500 <mnsahmed500@gmail.com>
2026-07-23 02:37:01 -0500

    update talk bot

--- Recommended curriculum/JavaScript Certification/JavaScript/Variables and Strings/Build a Greeting Bot/index.js
+++ Recommended curriculum/JavaScript Certification/JavaScript/Variables and Strings/Build a Greeting Bot/index.js
@@ -4,7 +4,6 @@ let bot;
 bot = "talkBot";
 
 let botLocation = "the kernal base";
-
 console.log("Allow me to introduce myself.");
 
 const botIntroduction = "My name is " + bot + ".";
@@ -14,23 +13,19 @@ const botLocationSentence = "I live in " + botLocation + ".";
 console.log(botLocationSentence);
 
 bot = "proTalkBot";
-
 const nicknameIntroduction = "My nickname is " + bot + ".";
 console.log(nicknameIntroduction);
 
 bot = "awesomeTalkBot";
-
 const newNicknameGreeting = "I love my nickname but I wish people would call me " + bot + ".";
 console.log(newNicknameGreeting);
 
 const favoriteSubject = "Computer Science & ethical hacking";
-
 const favoriteSubjectSentence = "My favorite subject is " + favoriteSubject + ".";
 console.log(favoriteSubjectSentence);
 console.log("Well, it was nice to talk to you. Have a nice day!");
 
 bot = 'studentTalkBot';
-
 const studentBotIntroduction = "My name is " + bot + ", I dedicated to achieve my goal!";
 console.log(studentBotIntroduction);
 console.log('final, i have complete today session as student...');
